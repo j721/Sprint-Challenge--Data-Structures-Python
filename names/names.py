da82d9c263bd9d982c81475eb7ec74157a8a896b
Julia Vu <juliadvu@gmail.com>
2020-06-12 10:38:07 -0500

    working on names.py file going to try using binary tree

--- names/names.py
+++ names/names.py
@@ -1,5 +1,91 @@
 import time
 
+#going to try Binary Tree
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        #compare the value to the root's value to determine which direction
+            #we're gonna go in
+            #if the value < root's value
+        if value < self.value:
+            #go left
+            #how do we go left
+            #we have to check if there is another node on the left side
+            if self.left:
+                self.left.insert(value)
+            else:
+                #then we can park the value here
+                self.left = BSTNode(value)
+        #else te value >= root's value
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                    #then we can park the value here
+                self.right = BSTNode(value)
+
+
+    # Return True if the tree contains the value
+    # False if it does not
+
+    def contains(self, target):
+        #base case?
+        #we find the target in the tree node
+        if self.value == target:
+            return True
+        #figure out which direction we need to go in
+        if target < self.value:
+            #we go left
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        #or, we get a spot where the node should be, but nothing is there
+        else: 
+            #we go right
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+        #how do we move towards the base case?
+                
+
+    # Return the maximum value found in the tree
+    #used recursion - function that calls itself
+    #run time O (log n)
+    def get_max(self):
+        #check to the right side of the tree
+        #since left (self.left.value) side of the tree will always be smaller than the root
+
+        #if the right side of the tree is empty that just return the tree
+        if not self.right:
+            return self.value
+        #if right side of the tree is not empty. Then get the right child node with the max value
+        else:
+            return self.right.get_max()
+
+
+    # Call the function `fn` on the value of each node
+
+    #example of a tree traversal. Want to traverse through every tree node
+    #recursion
+    #doesn't actually return anything 
+    def for_each(self, fn):
+       #call the function `fn` on self.value
+       fn(self.value)
+        #go to right child nodes if any exists
+       if self.right:
+           self.right.for_each(fn)
+       #go to the left child nodes if any exists
+       if self.left:
+           self.left.for_each(fn)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,10 +99,17 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+tree = BSTNode(names_1[0])
+for name in names_1:
+    tree.insert(name)
+for name2 in names_2:
+    if tree.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
