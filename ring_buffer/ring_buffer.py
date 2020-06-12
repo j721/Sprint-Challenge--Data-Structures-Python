33f28daab44d3a7fe7d74eec4a90350aa55a3cab
Julia Vu <juliadvu@gmail.com>
2020-06-12 10:08:51 -0500

    added notes into ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,9 @@
+#Notes
+# ring buffer is a non-growable buffer with a fixed size. 
+#also known as a circular Queue. FIFO (first in first out) order
+# When the ring buffer is full and a new element is inserted, 
+# the oldest element in the ring buffer is overwritten with the newest element. 
+
 class RingBuffer:
     def __init__(self, capacity):
         pass
